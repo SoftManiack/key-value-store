b525f93041a102709250456ee7354100c8f4a560
Suckhodoev Artem <fetelupercal@gmail.com>
2024-04-08 22:50:46 +0500

    transaction log

--- transation_logger.go
+++ transation_logger.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type EventType byte
 
@@ -19,7 +23,12 @@ type Event struct {
 
 type TransactionLogger interface {
 	WriteDelete(key string)
-	WritePut(key string)
+	WritePut(key, value string)
+	Err() <-chan error
+
+	ReadEvents() (<-chan Event, <-chan error)
+
+	Run()
 }
 
 type FileTransactionLogger struct {
@@ -39,4 +48,83 @@ func (l *FileTransactionLogger) WriteDelete(key string) {
 
 func (l *FileTransactionLogger) Err() <-chan error {
 	return l.errors
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
+	}
+
+	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		// извлеч след событие event
+		for e := range events {
+
+			l.lastSequense++ // Увеличить порядковый номер
+
+			_, err := fmt.Fprintf(
+				l.file,
+				"%d\t%d\t%s\t%s]n",
+				l.lastSequense, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+	outEvent := make(chan Event)    // Небуферизированный канал событий
+	outError := make(chan error, 1) // Буферезированный канал событий
+
+	go func() {
+		var e Event
+
+		defer close(outEvent)
+		defer close(outError)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s",
+				&e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+
+			// Проверка целостности
+			// Порядковые номера последовательно учеличиваются
+
+			if l.lastSequense >= e.Sequence {
+				outError <- fmt.Errorf("transaction numbers out of sequence")
+
+				return
+			}
+
+			l.lastSequense = e.Sequence
+
+			outEvent <- e // Отправляет событе along
+
+		}
+
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+		}
+	}()
+
+	return outEvent, outError
 }
